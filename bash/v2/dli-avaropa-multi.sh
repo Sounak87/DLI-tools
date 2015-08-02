24892c059e62c20f2c558dac5f11a0767509bc51
vvasuki <vishvas.vasuki@gmail.com>
2015-08-02 08:08:27 -0700

    asdf

--- bash/v2/dli-avaropa-multi.sh
+++ bash/v2/dli-avaropa-multi.sh
@@ -9,14 +9,14 @@
 #
 # USAGE: dli-avaropa-multi <filename>
 #        <filename> - file in which each line contains (only) a DLI barcode and local ad-hoc book name separated by whitespace
-
-[ "$#" != "1" ] && echo "USAGE: dli-avaropa-multi <filename>" >&2 && exit 1
+set -o verbose
+[ "$#" != "1" ] && echo "USAGE: dli-avaropa-multi.sh <filename>" >&2 && exit 1
 
 cat "$1" | while read barcode bookname
 do
 	if [ -z "$barcode" -o -z "$bookname" ] ; then
-		echo "dli-avaropa-multi ERROR: malformed input line: $barcode $bookname"
+		echo "dli-avaropa-multi.sh ERROR: malformed input line: $barcode $bookname"
 		continue
 	fi
-	dli-avaropa "$barcode" "$bookname"
+	~/DLI-tools/bash/v2/dli-avaropa-multi.sh "$barcode" $bookname
 done
